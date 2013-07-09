123e8ef38dfcc9188c302c402930687153c071a0
kiwi <jcwen@thoughtworks.com>
2013-07-09 13:01:56 +0800

    bimap learning tests

---
+++ src/test/java/org/kiwi/guava/CollectionsTest.java
@@ -0,0 +1,31 @@
+package org.kiwi.guava;
+
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class CollectionsTest {
+
+    private BiMap<String,Integer> userMap;
+
+    @Before
+    public void setUp() throws Exception {
+        userMap = HashBiMap.create();
+        userMap.put("A", 1);
+        userMap.put("B", 2);
+    }
+
+    @Test
+    public void test_get_value_by_key() {
+        assertThat(userMap.get("A"), is(1));
+    }
+
+    @Test
+    public void test_get_key_by_value() {
+        assertThat(userMap.inverse().get(1), is("A"));
+    }
+}
